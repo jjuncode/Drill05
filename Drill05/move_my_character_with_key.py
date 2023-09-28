35f1cae66b32dfa798d2dff31936e474d9eaa730
jjuncode <jjun.code@gmail.com>
2023-09-28 15:08:48 +0900

    draw cursor , character

--- Drill05/move_my_character_with_key.py
+++ Drill05/move_my_character_with_key.py
@@ -1,14 +1,17 @@
 from pico2d import *
 
-open_canvas(736,521)
-background = load_image('TUK_GROUND.png')
+TUK_WIDTH, TUK_HEIGHT = 800, 600
+open_canvas(TUK_WIDTH, TUK_HEIGHT)
+
+TUK_ground = load_image('TUK_GROUND.png')
+mouse = load_image('hand_arrow.png')
 character = load_image('sprite.png')
 
-center = get_canvas_width()/2,get_canvas_height()/2 # window center
-
-x,y = 400,center[1]   # character pos
+mouse_x, mouse_y = TUK_WIDTH // 2, TUK_HEIGHT // 2
+character_x,character_y = mouse_x,mouse_y
 move_speed = 15
 quit = False
+hide_cursor()
 
 class Ani:
     cur_frame =0
@@ -18,50 +21,44 @@ class Ani:
     cur_ani = 0         # cur ani ( from img_bottom )
 
 def DrawAnimation():
-    global x,y
+    global mouse_x,mouse_y,character_x,character_y
     clear_canvas()
-    background.draw(center[0],center[1])
+    TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
+    mouse.draw(mouse_x,mouse_y)
     MoveCharacter()
     character.clip_draw(Ani.cur_frame*Ani.offset_x,Ani.cur_ani*Ani.offset_y,Ani.offset_x,Ani.offset_y
-                        ,x,y)
+                        ,character_x,character_y)
     update_canvas()
  
 def SetFrame():
     Ani.cur_frame += 1
     if (Ani.cur_frame > Ani.max_frame[Ani.cur_ani]):    # Frame reset
         Ani.cur_frame =0
-
-    delay(0.1)
-
 def MoveCharacter():
     pass
 
 def handle_event():
-    global quit,dir
+    global quit,mouse_x,mouse_y
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             quit = True
-        if event.type == SDL_KEYDOWN:
+        elif event.type == SDL_KEYDOWN:
             if event.key == SDLK_ESCAPE:
                 quit=True
-            elif event.key ==SDLK_UP:
-                Ani.cur_ani = 1
-                Ani.cur_frame = 0
-            elif event.key == SDLK_DOWN:
-                Ani.cur_ani = 3
-                Ani.cur_frame = 0
-            elif event.key == SDLK_LEFT:
-                Ani.cur_ani = 2
-                Ani.cur_frame = 0
-            elif event.key == SDLK_RIGHT:
-                Ani.cur_ani = 0
-                Ani.cur_frame = 0
+        elif event.type == SDL_MOUSEMOTION:
+            mouse_x, mouse_y = event.x, TUK_HEIGHT - 1 - event.y
+    pass
 
-while (not quit):
-    SetFrame()
-    handle_event()
-    DrawAnimation()
+def main():
+    while (not quit):
+        SetFrame()
+        handle_event()
+        DrawAnimation()
+
+    close_canvas()
+
+main()
 
 
-close_canvas()
+
